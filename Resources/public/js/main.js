b222bb5acd8991ed286e8c09a1c74f7258b09e17
Mark <mark.woynarski@nobletdesign.com>
2013-02-22 17:17:31 -0600

    Tweaked alert message style, js

---
+++ Resources/public/js/main.js
@@ -0,0 +1,22 @@
+Event.observe(window, 'load', function(event)
+{
+    var messages = $$('div.bannerMessage');
+    if(messages)
+    {
+        messages.each(function(m)
+        {
+            var b = m.down('button');
+            b.observe('click', function()
+            {
+                new Effect.Morph(m,{
+                    style: 'height:0; opacity: 0;',
+                    duration: 0.8,
+                    afterFinish: function()
+                    {
+                        m.remove()
+                    }
+                });
+            });
+        });
+    }
+});
