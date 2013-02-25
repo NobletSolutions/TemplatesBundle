1e35e10e0bf4fff579470cc2431d79098d7e1935
Nathanael D. Noblet <nathanael@gnat.ca>
2013-02-25 11:01:02 -0700

    Flash messages

---
+++ Model/FlashMessage.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace NS\TemplatesBundle\Model;
+
+class FlashMessage
+{
+    private $header;
+    private $title;
+    private $message;
+    private $buttonMessage;
+    
+    public function __construct($header = null, $title = null, $message = null, $buttonMessage = null)
+    {
+        $this->title         = $title;
+        $this->message       = $message;
+        $this->buttonMessage = $buttonMessage;
+    }
+    
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    public function getButtonMessage()
+    {
+        return $this->buttonMessage;
+    }
+
+    public function setButtonMessage($buttonMessage)
+    {
+        $this->buttonMessage = $buttonMessage;
+    }
+
+    public function getHeader()
+    {
+        return $this->header;
+    }
+
+    public function setHeader($header)
+    {
+        $this->header = $header;
+    }
+}
