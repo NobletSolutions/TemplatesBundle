1b7400c9107cbdea842b5407ed1cb05b1ef3c2e5
Mark <mark.woynarski@nobletdesign.com>
2013-05-31 11:37:16 -0600

    Added generic collapseable panel

--- Resources/public/js/main.js
+++ Resources/public/js/main.js
@@ -2,24 +2,6 @@ Event.observe(window, 'load', function(event)
 {
     activateFilters();
     activateSessionMenu();
-    
-    var messages = $$('div.bannerMessage');
-    if(messages)
-    {
-        messages.each(function(m)
-        {
-            var b = m.down('button');
-            b.observe('click', function()
-            {
-                new Effect.Morph(m,{
-                    style: 'height:0; opacity: 0;',
-                    duration: 0.8,
-                    afterFinish: function()
-                    {
-                        m.remove();
-                    }
-                });
-            });
-        });
-    }
+    activateMessages();
+    activatePanelSwitches();
 });
---
+++ Resources/public/js/panels.js
@@ -0,0 +1,52 @@
+var activateMessages = function()
+{
+    var messages = $$('div.bannerMessage');
+    if(messages)
+    {
+        messages.each(function(m)
+        {
+            var b = m.down('button');
+            b.observe('click', function()
+            {
+                new Effect.Morph(m,{
+                    style: 'height:0; opacity: 0;',
+                    duration: 0.8,
+                    afterFinish: function()
+                    {
+                        m.remove();
+                    }
+                });
+            });
+        });
+    }    
+};
+
+var activatePanelSwitches = function()
+{
+    $$('a.panelSwitch').each(function(a)
+    {
+        var panel = $(a.getAttribute('data-target'));
+        
+        if(panel)
+        {
+            a.observe('click', function(ev)
+            {
+                options = {
+                    duration: 0.5,
+                    afterFinish: function()
+                    {
+                        panel.toggleClassName('open');
+                        a.toggleClassName('open');
+                    }
+                };
+                
+                if(panel.hasClassName('open'))
+                    Effect.BlindUp(panel, options);
+                else
+                    Effect.BlindDown(panel, options);
+            });
+            
+            panel.hide();
+        }
+    });
+};
