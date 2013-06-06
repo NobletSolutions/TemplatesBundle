8f5768e937a6f75125417476536de45e4a53968b
Nathanael D. Noblet <nathanael@gnat.ca>
2013-06-06 12:14:30 -0600

    fix the date picker to deal with incoming DateTime values

--- Form/DatePickerType.php
+++ Form/DatePickerType.php
@@ -2,7 +2,10 @@
 
 namespace NS\TemplatesBundle\Form;
 
+use \Symfony\Component\Form\Extension\Core\Type\DateType;
 use \Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeToLocalizedStringTransformer;
 
 /**
  * A Date picker text type
@@ -11,6 +14,25 @@ use \Symfony\Component\Form\Extension\Core\Type\TextType;
  */
 class DatePickerType extends TextType
 {
+    const DEFAULT_FORMAT = \IntlDateFormatter::MEDIUM;
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $dateFormat = self::DEFAULT_FORMAT;
+        $timeFormat = \IntlDateFormatter::NONE;
+        $calendar   = \IntlDateFormatter::GREGORIAN;
+        $pattern    = 'Y-M-d';
+        
+        $builder->addViewTransformer(new DateTimeToLocalizedStringTransformer(
+                null,
+                null,
+                $dateFormat,
+                $timeFormat,
+                $calendar,
+                $pattern
+            ));
+    }
+
     public function getName()
     {
         return 'datepicker';
