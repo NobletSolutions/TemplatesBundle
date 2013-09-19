998ab0cb1b130ea6c7ac0ec68a10dd43861e3857
Mark <mark.woynarski@nobletdesign.com>
2013-09-19 14:20:02 -0600

    Added Management Report Call info

--- Resources/public/js/panels.js
+++ Resources/public/js/panels.js
@@ -46,14 +46,44 @@ var activatePanelSwitches = function()
                         Effect.BlindUp(panel, options);
                     else
                         Effect.BlindDown(panel, options);
+                    
+                    Event.stop(ev);
+                    return false;
                 });
                 
                 if(!panel.hasClassName('open'))
                     panel.hide();
             }
             a.activated = true;
-        }
-        
+        }        
+    });
+    
+    $$('a.panelUpdater').each(function(a)
+    {
+       if(!a.activated)
+       {
+           var target = $(a.getAttribute('data-target'));
+           var source = $(a.getAttribute('data-source'));
+           
+           if(target && source)
+           {
+               a.observe('click', function(ev)
+               {
+                   if(target.open)
+                       target.source.update(target.down());
+                   
+                   target.update(source.down());
+                   target.source = source;
+                   
+                   target.open = true;
+                   
+                   Event.stop(ev);
+                   return false;
+               });
+           }
+           
+           a.activated = true;
+       }
     });
 };
 
